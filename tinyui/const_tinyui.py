d1c1552e09c6885066d9ffb82b2e82964429e902
Oost-hash <rroeten@gmail.com>
2026-03-06 18:35:19 +0100

    refactor: import version from version.py, set min TinyPedal to 2.42.0

--- tinyui/const_tinyui.py
+++ tinyui/const_tinyui.py
@@ -3,13 +3,14 @@
 #  Copyright (C) 2025 Oost-hash
 #
 
-"""TinyUi constanten"""
+"""TinyUi constants"""
+
+from .version import __version__, DEVELOPMENT
 
 APP_NAME = "TinyUi"
-VERSION = "0.2.0"
 AUTHOR = "Oost-hash"
 LICENSE = "GPLv3"
 URL = "https://github.com/Oost-hash/TinyUi"
 
-# Minimum TinyPedal versie die we ondersteunen
-MIN_TINYPEDAL_VERSION = "2.5.0"
+# Minimum TinyPedal version supported
+MIN_TINYPEDAL_VERSION = "2.42.0"
