873ee5212122cd835db4d83af81962303296739b
Oost-hash <rroeten@gmail.com>
2026-03-10 16:17:22 +0100

    changed init too use lazy and config adapter

--- tinyui/backend/__init__.py
+++ tinyui/backend/__init__.py
@@ -1,16 +1,33 @@
-"""
-TinyPedal backend adapter layer.
+"""TinyPedal backend adapter layer."""
 
-All tinypedal imports used by tinyui are centralized here.
-If tinypedal changes, only this package needs updating.
-"""
+from .config_adapter import ConfigAdapter
+from .lazy import LazyModule
+from .settings import (
+    copy_setting,
+    load_setting_json_file,
+    save_and_verify_json_file,
+)
 
-# Re-export all adapter modules
+config = ConfigAdapter()
+
+
+def init_backend(real_cfg):
+    config.inject_real(real_cfg)
+
+
+# Lazy proxies voor TinyPedal modules
+api = LazyModule("tinypedal_repo.tinypedal.api_control", "api")
+loader = LazyModule("tinypedal_repo.tinypedal.loader")
+kctrl = LazyModule("tinypedal_repo.tinypedal.hotkey_control", "kctrl")
+mctrl = LazyModule("tinypedal_repo.tinypedal.module_control", "mctrl")
+octrl = LazyModule("tinypedal_repo.tinypedal.overlay_control", "octrl")
+wctrl = LazyModule("tinypedal_repo.tinypedal.module_control", "wctrl")
+update_checker = LazyModule("tinypedal_repo.tinypedal.update", "update_checker")
+minfo = LazyModule("tinypedal_repo.tinypedal.module_info", "minfo")
+calc = LazyModule("tinypedal_repo.tinypedal.calculation")
+units = LazyModule("tinypedal_repo.tinypedal.units")
+realtime_state = LazyModule("tinypedal_repo.tinypedal.realtime_state")
+
+# Exporteer ook de minimale gegenereerde modules
 from .constants import *
-from .controls import *
-from .data import *
-from .formatter import *
 from .misc import *
-from .regex import *
-from .settings import *
-from .validator import *
