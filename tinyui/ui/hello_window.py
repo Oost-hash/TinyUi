8009301ea593bc9463aa7ea60d7041e8a2ebeaf8
Oost-hash <rroeten@gmail.com>
2026-03-10 20:59:37 +0100

    feat: add button too heatmap editor

--- tinyui/ui/hello_window.py
+++ tinyui/ui/hello_window.py
@@ -8,7 +8,9 @@ from PySide2.QtWidgets import (
     QWidget,
 )
 
-from tinyui.adapters import cfg
+from tinyui.adapters import cfg, lifecycle
+
+from .editors.heatmap import HeatmapEditor
 
 
 class HelloWindow(QMainWindow):
@@ -30,10 +32,25 @@ class HelloWindow(QMainWindow):
         label = QLabel(f"Hallo! API = {api_name}")
         layout.addWidget(label)
 
+        # Knop om HeatmapEditor te openen
+        self.open_heatmap_btn = QPushButton("Open Heatmap Editor")
+        self.open_heatmap_btn.clicked.connect(self.open_heatmap_editor)
+        layout.addWidget(self.open_heatmap_btn)
+
         # Afsluitknop
         quit_btn = QPushButton("Close")
         quit_btn.clicked.connect(self._quit)
         layout.addWidget(quit_btn)
+
+        self.heatmap_editor = None
+
+    def open_heatmap_editor(self):
+        """Opent het HeatmapEditor-venster."""
+        if self.heatmap_editor is None or not self.heatmap_editor.isVisible():
+            self.heatmap_editor = HeatmapEditor()
+            self.heatmap_editor.show()
+        else:
+            self.heatmap_editor.raise_()
 
     def closeEvent(self, event):
         """Overschrijf kruisje-gedrag: sluit hele applicatie."""
