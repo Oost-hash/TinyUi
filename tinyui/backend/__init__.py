9123d74652d059bff5ef667fd3e65d015c322aea
Oost-hash <rroeten@gmail.com>
2026-03-10 13:11:57 +0100

    fix: Namespace issue
    Changed submodule from tinypedal to tinypedal_repo

--- tinyui/backend/__init__.py
+++ tinyui/backend/__init__.py
@@ -4,3 +4,13 @@ TinyPedal backend adapter layer.
 All tinypedal imports used by tinyui are centralized here.
 If tinypedal changes, only this package needs updating.
 """
+
+# Re-export all adapter modules
+from .constants import *
+from .controls import *
+from .data import *
+from .formatter import *
+from .misc import *
+from .regex import *
+from .settings import *
+from .validator import *
