c722ed7a6e145c7e08bc66b3a2a40eb30b6c940a
Oost-hash <rroeten@gmail.com>
2026-03-10 23:50:04 +0100

    chore: BANNERS, BANNERS, BANNERRSSSSS
    Why did you do this yourself? TOKENS my boiii, tokens

--- tinyui/ui/tabs/__init__.py
+++ tinyui/ui/tabs/__init__.py
@@ -1,3 +1,24 @@
+#  TinyUI - A mod for TinyPedal
+#  Copyright (C) 2026 Oost-hash
+#
+#  This file is part of TinyUI.
+#
+#  TinyUI is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  TinyUI is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  TinyUI builds on TinyPedal by s-victor (https://github.com/s-victor/TinyPedal),
+#  licensed under GPLv3. TinyPedal is included as a submodule.
+
 # tinyui/ui/tabs/__init__.py
 from .module_tab.module_tab_view import ModuleTabView
 from .module_tab.module_tab_viewmodel import ModuleTabViewModel
