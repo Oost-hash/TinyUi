d2a5b755ff632eabf002ab7f2059a219a7abcf77
Oost-hash <rroeten@gmail.com>
2026-03-16 21:33:01 +0100

    remove old import

--- src/tinyui/ui/editors/__init__.py
+++ src/tinyui/ui/editors/__init__.py
@@ -19,4 +19,3 @@
 #  TinyUI builds on TinyPedal by s-victor (https://github.com/s-victor/TinyPedal),
 #  licensed under GPLv3. TinyPedal is included as a submodule.
 
-from .heatmap import HeatmapEditor
