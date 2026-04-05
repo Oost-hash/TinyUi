2082a66ce45388c8ca4d9fe1c7eb18aac58882ab
Oost-hash <rroeten@gmail.com>
2026-04-06 01:53:39 +0200

    feat(runtime): window runtime domain for window state tracking

---
+++ src/runtime/windows/__init__.py
@@ -0,0 +1,36 @@
+#  TinyUI
+#  Copyright (C) 2026 Oost-hash
+#
+#  This file is part of TinyUI.
+#
+#  TinyUI is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  TinyUI is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  TinyUI builds on TinyPedal by s-victor (https://github.com/s-victor/TinyPedal),
+#  licensed under GPLv3.
+
+"""Window runtime domain — manages window state and lifecycle."""
+
+from runtime.windows.runtime import WindowRuntime
+from runtime.windows.startup import (
+    WindowRuntimeStartupResult,
+    get_window_runtime_result,
+    startup_window_runtime,
+)
+
+__all__ = [
+    "WindowRuntime",
+    "WindowRuntimeStartupResult",
+    "get_window_runtime_result",
+    "startup_window_runtime",
+]
